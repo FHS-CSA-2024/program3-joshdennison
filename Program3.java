4cec3795c690623c8e0d07965c6cb47ad4b4af70
joshdennison <jden3.14@gmail.com>
2024-08-13 14:05:19 -0700

    Update Program3.java

--- Program3.java
+++ Program3.java
@@ -3,11 +3,14 @@
 import java.util.Scanner; //This imports the console scanner that reads user input
 
 //Your code here:
-public class main {
+ class program3thing {
 public static void main(String[] args){
-
-
-
+int width= 9;
+  int height = 1;
+Scanner input = new Scanner(System.in);
+System.out.print("enter width");
+  System.out.print("enter height");
+width= Input();
 //Paste console output below:
 /*
 
